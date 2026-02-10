670a51daf0ddf84921d8aef3c14d6ed76e156ec8
dinoDuvnjak <dinoDuvnjak@users.noreply.github.com>
2026-02-10 06:44:47 +0100

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'loko-voznje-v1';
+const urlsToCache = [
+  './loko-voznje.html',
+  './manifest.json'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
